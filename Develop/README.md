937517bc60f4a13020981a3fc23099c9914e9e14
ElBoyTM <nszalenski@gmail.com>
2024-10-04 12:13:44 -0400

    added licenseSection

---
+++ Develop/README.md
@@ -0,0 +1,34 @@
+# 1
+
+  ## Description
+  1
+
+  ## Table of Contents
+  [Installation](#installation)
+
+  [Usage](#usage)
+
+  
+  [Contributing](#contributing)
+
+  [Tests](#tests)
+
+  [Questions](#questions)
+
+  ## Installation
+  1
+
+  ## Usage
+  1
+
+  
+  ## Contributing
+  1
+
+  ## Tests
+  1
+
+  ## Questions
+  My GitHub: 1
+
+  My email: 1
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -14,11 +14,20 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return `## License
+    
+    `;
+  } else {
+    return ``;
+  };
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   const licenseLink = renderLicenseLink(data.license);
+  const licenseSection = renderLicenseSection(data.license);
   return `# ${data.title}
 
   ## Description
@@ -38,8 +47,7 @@ function generateMarkdown(data) {
   ## Usage
   ${data.usage}
 
-  ## License
-
+  ${licenseSection}
   ## Contributing
   ${data.contributing}
 

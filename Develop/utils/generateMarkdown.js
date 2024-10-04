c37ca5490db538bad0e813c256401c70c9612a02
ElBoyTM <nszalenski@gmail.com>
2024-10-04 13:07:22 -0400

    added username to license

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,10 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license === 'None') {
+  if (license.license === 'None') {
     return ``;
-  } else if (license === 'MIT') {
-    return `Copyright (c) ${new Date().getFullYear()} <copyright holders>
+  } else if (license.license === 'MIT') {
+    return `Copyright (c) ${new Date().getFullYear()} ${license.username}
 
     Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -29,7 +29,7 @@ function renderLicenseLink(license) {
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   const licenseBadge = renderLicenseBadge(license);
-  if (license !== 'None') {
+  if (license.license !== 'None') {
     return `
 
   ## License
@@ -42,7 +42,7 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   const licenseLink = renderLicenseLink(data.license);
-  const licenseSection = renderLicenseSection(data.license);
+  const licenseSection = renderLicenseSection(data);
   return `# ${data.title}
 
   ## Description
